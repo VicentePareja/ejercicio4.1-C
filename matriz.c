8ffb37a688928662ca10751d25e48d0497ad9757
Vicente Pareja <vicente.pareja@uc.cl>
2024-03-23 18:14:21 -0300

    Generalización de tamaño de matriz implementada

--- matriz.c
+++ matriz.c
@@ -6,31 +6,46 @@ typedef struct celda_t{
     int y;
 } Celda;
 
-
-int main (int argc, char **argv){
-
-    Celda** Matriz = calloc(3, sizeof(Celda*));
-
-    for (int i = 0; i < 3; i++){
-        Matriz[i] = calloc(3, sizeof(Celda));
+int imprimir_matriz (Celda ** Matriz, int rows, int columns){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            printf("%d , %d | ", Matriz[i][j].x, Matriz[i][j].y);
+        }
+        printf("\n");
     }
+}
 
-    for (int i = 0; i < 3; i++){
+int rellenar_con_celdas(Celda **Matriz, int rows, int columns){
+    for (int i = 0; i < rows; i++){
 
-        for (int j = 0; j < 3; j++){
+        for (int j = 0; j < columns; j++){
             Celda nueva_celda;
             nueva_celda.x = i;
             nueva_celda.y = j;
             Matriz[i][j] = nueva_celda;
         }
     }
+}
 
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            printf("%d , %d | ", Matriz[i][j].x, Matriz[i][j].y);
-        }
-        printf("\n");
+
+int main (int argc, char **argv){
+
+    int rows;
+    int columns;
+
+    printf("Enter the size of your Matrix: ");
+    scanf("%d %d", &rows, &columns);
+    printf("%d %d\n", rows, columns);
+
+    Celda** Matriz = calloc(rows, sizeof(Celda*));
+
+    for (int i = 0; i < rows; i++){
+        Matriz[i] = calloc(columns, sizeof(Celda));
     }
+
+    
+
+    imprimir_matriz(Matriz, rows, columns);
 
     for (int i = 0; i < 3; i++){
         free(Matriz[i]);
